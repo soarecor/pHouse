0ac11d83579dfdcc96f080529a6d9bd4dac0d53a
Darren Hebner <darrenwilliamhebner@gmail.com>
2016-04-08 12:39:28 -0400

    Updated localhost port. Sorry!

--- app/php/getPerson.php
+++ app/php/getPerson.php
@@ -1,5 +1,5 @@
 <?php
-	$db = new PDO('mysql:host=localhost:3306;dbname=shertv_SPH-DBFinal;charset=utf8', 'root', 'root');
+	$db = new PDO('mysql:host=localhost:8889;dbname=shertv_SPH-DBFinal;charset=utf8', 'root', 'root');
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
 	$projectID = ($_GET['id']);
